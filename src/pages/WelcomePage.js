973e8820a6391136d7cec6ee61711d7dc62eebc5
chanhee7 <chan9505@naver.com>
2024-07-16 09:57:12 +0900

    # 로그인 조건부 렌더링

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import LoginForm from '../components/auth/LoginForm'
+import React, { useEffect, useState } from "react";
+import LoginForm from "../components/auth/LoginForm";
+import Main from "../components/Main";
 
-const WelcomPage = () => {
-  return <LoginForm />
-}
+const WelcomePage = () => {
 
-export default WelcomPage
+  console.log('WelcomPage 실행');
+
+  return (
+    <>
+      {<LoginForm />}
+      {<Main />}
+    </>
+  );
+};
+
+export default WelcomePage;
